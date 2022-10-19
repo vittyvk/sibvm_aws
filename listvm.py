9f3f0c3a524833c5d76fe01ed8365d2c05e8cbdd
Vitaly Kuznetsov <vkuznets@redhat.com>
2022-10-19 16:49:40 +0200

    Skip VMs in 'shutting down' state
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- listvm.py
+++ listvm.py
@@ -4,5 +4,5 @@ import aws
 
 if __name__ == '__main__':
     for instance in aws.get_instances():
-        if instance.state['Name'] != "terminated":
+        if instance.state['Name'] not in ("terminated", "shutting-down"):
             print(instance.id, instance.instance_type)
